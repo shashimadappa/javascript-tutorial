f6b663ba567121dfee14cee18decbbbffb12bdff
shashimadappa <iamshashi321@gmail.com>
2025-07-20 18:46:36 +0530

    times and event loops

---
+++ my-js-project/src/set.js
@@ -0,0 +1,56 @@
+// [ Call Stack ]
+//       ↓
+// [ process.nextTick Queue ] <-- microtask queue
+//       ↓
+// [ Other Microtasks (e.g., Promises) ]
+//       ↓
+// [ Timers (setTimeout, setInterval) ]
+//       ↓
+// [ I/O callbacks ]
+//       ↓
+// [ setImmediate Queue ]
+
+console.log("Start");
+
+setTimeout(() => {
+  console.log("setTimeout");
+}, 0);
+
+setImmediate(() => {
+  console.log("setImmediate");
+});
+
+process.nextTick(() => {
+  console.log("process.nextTick");
+});
+
+console.log("End");
+// Start
+// End
+// process.nextTick
+// setTimeout
+// setImmediate
+
+
+console.log("Start");
+
+setTimeout(() => {
+  console.log("setTimeout");
+}, 5000); 
+
+setImmediate(() => {
+  console.log("setImmediate");
+});
+
+process.nextTick(() => {
+  console.log("process.nextTick");
+});
+
+console.log("End");
+// Start
+// End
+// process.nextTick
+// setImmediate
+// (setTimeout after 5 seconds)
+// setTimeout
+
